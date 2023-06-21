a1b73e46362af364209acc0f9654010bc5f26b2d
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-06-21 12:30:34 +0200

    add branch update script

---
+++ update-branches.sh
@@ -0,0 +1,29 @@
+set -e
+
+git checkout 1-init
+
+branches=(
+  1-init
+  2-theme 2-theme-solution
+  3-resumability
+)
+previous=
+current=
+
+for branch in ${branches[*]}; do
+  previous=$current
+  current=$next
+  next=$branch
+
+  if [ ! $current = "" ]
+  then
+    git checkout $current
+    git tag -d `git tag | grep -E '.'`
+    git merge $previous -m merge
+  fi;
+done
+
+git checkout $branch
+git merge $current $branch -m merge
+
+git checkout 1-init
